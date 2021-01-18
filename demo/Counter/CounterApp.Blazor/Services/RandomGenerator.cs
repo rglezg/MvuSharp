5cd7944b086f7b85adf5f20360721cfd6797910d
Renier González <renierglez97@gmail.com>
2021-01-17 20:24:05 -0500

    Added missing files to git.

---
+++ demo/Counter/CounterApp.Blazor/Services/RandomGenerator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CounterApp.Blazor.Services
+{
+    public class RandomGenerator
+    {
+        private readonly Random _random = new();
+        
+        public int RandomInt(int min, int max)
+        {
+            return _random.Next(min, max);
+        }
+    }
+}
---
+++ demo/Counter/CounterApp.Tests/CounterTests.cs
@@ -0,0 +1,58 @@
+using System;
+using CounterApp.Core;
+using MvuSharp.Testing;
+using Xunit;
+using System.Linq;
+using static CounterApp.Core.Counter;
+
+namespace CounterApp.Tests
+{
+    public class CounterTests
+    {
+        private static readonly Model DefaultModel = new (0, false);
+        
+        [Fact(DisplayName = "'Increment' increases 'Count' by 1")]
+        public void Increment()
+        {
+            var model = DefaultModel;
+            new MockBuilder()
+                .BuildMediator()
+                .TestMvuFunc(Update(model, new Msg.Increment()),
+                    m => Assert.Equal(model with {Count = 1}, m));
+        }
+        
+        [Fact(DisplayName = "'Decrement' decreases 'Count' by 1")]
+        public void Decrement()
+        {
+            var model = DefaultModel;
+            new MockBuilder()
+                .BuildMediator()
+                .TestMvuFunc(Update(model, new Msg.Decrement()),
+                    m => Assert.Equal(model with {Count = -1}, m));
+        }
+
+        [Fact(DisplayName = "'Random' sets Working to 'true' and dispatches 'Set' with a generated random number")]
+        public void Random()
+        {
+            const int random = 5;
+            var model = DefaultModel;
+            new MockBuilder()
+                .Setup<Request.RandomInt, int>(_ => random)
+                .BuildMediator()
+                .TestMvuFunc(Update(model, new Msg.Random()),
+                    m => Assert.Equal(model with {Working = true}, m),
+                    msg => Assert.Equal(new Msg.Set(random), msg.Single()));
+        }
+
+        [Fact(DisplayName = "'Set' sets 'Count' to the specified value and 'Working' to 'false'")]
+        public void Set()
+        {
+            const int random = 5;
+            var model = DefaultModel with {Working = true};
+            new MockBuilder()
+                .BuildMediator()
+                .TestMvuFunc(Update(model, new Msg.Set(random)),
+                    m => Assert.Equal(model with {Count = random, Working = false}, m));
+        }
+    }
+}
