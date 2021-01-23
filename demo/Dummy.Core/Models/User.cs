b20b0edf468886ab3e9e558a7bfc69e0a489ef10
Renier González <renierglez97@gmail.com>
2021-01-23 17:21:44 -0500

    Removed use of mutable structures.

--- demo/Dummy.Core/Models/User.cs
+++ demo/Dummy.Core/Models/User.cs
@@ -2,11 +2,5 @@
 
 namespace Dummy.Core.Models
 {
-    public class User
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public int Age { get; set; }
-    }
+    public record User(int Id, string Name, string Email, int Age);
 }
